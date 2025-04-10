57ef5bb48ffbbda6594135d6288ff5b81fd332fe
sakshi <sakshibule07@gmail.com>
2025-04-10 11:00:56 +0530

    Time calculation

---
+++ src/StringHandling/TimeCal.java
@@ -0,0 +1,19 @@
+package StringHandling;
+
+public class TimeCal {
+
+	public static void main(String[] args) {
+	
+		long start,end;
+		
+		start = System.nanoTime();
+		String str = "Welcome To ";
+		for(int i=1; i<=1000; i++) {
+			str+= "Logipool";
+			
+		}
+		end = System.nanoTime();
+		System.out.println((end-start));
+	}
+
+}
